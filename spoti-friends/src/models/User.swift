02cdce360f4e9d384db7abfb81be9ead9d2d8347
Yousuf Hassan <yousufhassan209@gmail.com>
2024-06-03 20:10:17 -0700

    Created User class with only id and authorizationCode attributes for now

---
+++ spoti-friends/src/models/User.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+class User {
+    var _id: UUID
+//    var spotifyProfile: SpotifyProfile
+//    var friends: List<SpotifyProfile>
+//    var spDcCookie: String
+//    var webAccessToken: SpotifyWebAccessToken
+    var authorizationCode: String
+    
+    init() {
+        self._id = UUID()
+//        self.spotifyProfile = SpotifyProfile(...)  // dummy function for example
+//        self.friends = self.getFriendList(...)  // dummy function for example
+//        self.spDcCookie = ""
+        self.authorizationCode = ""
+    }
+    
+    // List of methods to implement when the time comes
+    //    getSpotifyProfile()
+    //
+    //    getFriendList()
+    //    setFriendList()
+    //
+    //    isSpDcValid()
+    //    setSpDcAsValid()
+    //    setSpDcAsExpired()
+}
+
+
+//struct SpotifyWebAccessToken {
+//    var token: String
+//    var expiresAt: TimeInterval
+//}
