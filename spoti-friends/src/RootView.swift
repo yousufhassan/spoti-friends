24168fd7d98d443c7edc61ac6f6b74355b682347
Yousuf Hassan <yousufhassan209@gmail.com>
2024-06-07 22:19:47 -0700

    Updated user viewmodel to notify View of changes to the User object.
    View now navigates correctly based on authorization status.

--- spoti-friends/src/RootView.swift
+++ spoti-friends/src/RootView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import RealmSwift
 
 
 struct RootView: View {
--- spoti-friends/src/UserViewModel.swift
+++ spoti-friends/src/UserViewModel.swift
@@ -4,6 +4,7 @@ import RealmSwift
 
 class UserViewModel: ObservableObject {
     @Published var user: User
+    private var notificationToken: NotificationToken?
     
     init() {
         self.user = User()
@@ -13,6 +14,10 @@ class UserViewModel: ObservableObject {
                 realm.add(self.user)
             }
             print("Created user in VM!")
+            
+            self.notificationToken = realm.observe { [weak self] _, _ in
+                self?.objectWillChange.send()
+            }
         }
         //        let realm = try! Realm()
         //        if let existingUser = realm.objects(User.self).first {
